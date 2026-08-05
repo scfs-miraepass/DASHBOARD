7f0eff4649790e2b3492fcdf5b84b6f7562175ed
rldn12134 <dyddns101213@naver.com>
2026-08-05 04:26:12 +0000

    feat: 로그아웃 추가

---
+++ app/pages/logout.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { logoutAuthLogoutPost } from "@/sdk";
+
+definePageMeta({
+    layout: false
+})
+const session = useSession()
+const { lastFetched } = useAuth()
+
+onMounted(async () => {
+    const req = await logoutAuthLogoutPost()
+    if (!req.error) {
+        session.value = undefined
+        lastFetched.value = Date.now()
+        await navigateTo("/login", { replace: true })
+    }
+})
+</script>
+
+<template>
+    <div class="w-screen h-screen flex items-center justify-center text-p1">
+        <USkeleton class="bg-transparent flex flex-col items-center">
+            <LogoSymbol class="mb-2" />
+            로그아웃 중이에요.. 잠시만 기다려주세요.
+        </USkeleton>
+    </div>
+</template>
